5947335434fbbc6163f559091ee83a0d70acd07a
facundopareja <facundopareja@gmail.com>
2023-09-23 18:53:09 -0300

    fix: corrected target

---
+++ client/main.py
@@ -0,0 +1,11 @@
+"""Sample python script."""
+
+
+def print_hi(name):
+    """Docstring so lint doesnt annoy me."""
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
