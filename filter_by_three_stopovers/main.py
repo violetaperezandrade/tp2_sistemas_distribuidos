f3d36652ee8a9ca8bae0e53a190acd8a1655411e
violeta <viperez@fi.uba.ar>
2023-10-06 02:46:46 -0300

    merge

--- filter_by_three_stopovers/main.py
+++ filter_by_three_stopovers/main.py
@@ -11,17 +11,26 @@ def initialize_config():
     config.read("config.ini")
     config_params = {}
     try:
-        config_params["query_number"] = os.getenv('QUERY_NUMBER', config["DEFAULT"]["QUERY_NUMBER"])
-        config_params["output_queue"] = os.getenv('OUTPUT_QUEUE', config["DEFAULT"]["OUTPUT_QUEUE"])
-        config_params["input_queue"] = os.getenv('INPUT_QUEUE', config["DEFAULT"]["INPUT_QUEUE"])
-        config_params["logging_level"] = os.getenv('LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
-        config_params["max_stopovers"] = int(os.getenv('MAX_STOPOVERS', config["DEFAULT"]["MAX_STOPOVERS"]))
-        config_params["column_name"] = os.getenv('COLUMN_NAME', config["DEFAULT"]["COLUMN_NAME"])
-        config_params["columns_to_filter"] = os.getenv('COLUMNS_TO_FILTER', config["DEFAULT"]["COLUMNS_TO_FILTER"]).split(",")
+        config_params["query_number"] = os.getenv(
+            'QUERY_NUMBER', config["DEFAULT"]["QUERY_NUMBER"])
+        config_params["output_queue"] = os.getenv(
+            'OUTPUT_QUEUE', config["DEFAULT"]["OUTPUT_QUEUE"])
+        config_params["input_queue"] = os.getenv(
+            'INPUT_QUEUE', config["DEFAULT"]["INPUT_QUEUE"])
+        config_params["logging_level"] = os.getenv(
+            'LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
+        config_params["max_stopovers"] = int(
+            os.getenv('MAX_STOPOVERS', config["DEFAULT"]["MAX_STOPOVERS"]))
+        config_params["column_name"] = os.getenv(
+            'COLUMN_NAME', config["DEFAULT"]["COLUMN_NAME"])
+        config_params["columns_to_filter"] = os.getenv(
+            'COLUMNS_TO_FILTER', config["DEFAULT"]["COLUMNS_TO_FILTER"]).split(",")
     except KeyError as e:
-        raise KeyError("Key was not found. Error: {} .Aborting client".format(e))
+        raise KeyError(
+            "Key was not found. Error: {} .Aborting client".format(e))
     except ValueError as e:
-        raise ValueError("Key could not be parsed. Error: {}. Aborting client".format(e))
+        raise ValueError(
+            "Key could not be parsed. Error: {}. Aborting client".format(e))
 
     return config_params
 
@@ -46,6 +55,7 @@ def main():
     subscribe_to(connection.channel(), input_queue, filterByStopOvers.callback,
                  "filter_stopovers_queue")
     connection.close()
+
 
 
 if __name__ == '__main__':
